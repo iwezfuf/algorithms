00bc147cee6e93ef44cbce2b62bb89f48daef45c
iwezfuf <80110958+iwezfuf@users.noreply.github.com>
2022-01-06 15:17:43 +0100

    Update diamondSquare.py

--- drawing/heightmaps/diamondSquare.py
+++ drawing/heightmaps/diamondSquare.py
@@ -6,21 +6,25 @@ height = 500
 canvas = tkinter.Canvas(width=width,height=height)
 canvas.pack()
 
-heightmap = [[[] for j in range(129)] for i in range(129)]
-heightmap[0][0], heightmap[0][128], heightmap[128][128], heightmap[128][0] = 128
+size = 5 # musi byt 2^n + 1
 
-def diamondSquareAlgorithm(area, randomness):
-    ld, lh, pd, ph = area
-    middle = [abs(ld[0]-ph[0]), abs(ld[1]-ph[1])]
-    area
-    if middle[0] != int(middle[0]):
-        return
-    if middle[1] != int(middle[1]):
-        return
-    diamondSquareAlgorithm((ld,lh), randomness//2)
-    diamondSquareAlgorithm((ld,pd), randomness//2)
-    diamondSquareAlgorithm((pd,ph), randomness//2)
-    diamondSquareAlgorithm((ph,lh), randomness//2)
+heightmap = [[[] for j in range(size)] for i in range(size)]
+heightmap[0][0], heightmap[0][size-1], heightmap[size-1][size-1], heightmap[size-1][0] = 128, 128, 128, 128
+
+def diamondSquareAlgorithm(heightmap, start=(0,0), end=(len(heightmap)-1, len(heightmap)-1), randomness=0.1):
+    middle = int((end[0]-start[0])/2)
+    print(heightmap, start, end, middle)
+    heightmap[middle][middle] = (heightmap[start[0]][start[1]]+heightmap[start[0]][end[1]]+heightmap[end[0]][start[1]]+heightmap[end[0]][start[0]])/4
+    print(heightmap)
+    
+##    if middle[0] != int(middle[0]):
+##        return
+##    if middle[1] != int(middle[1]):
+##        return
+    diamondSquareAlgorithm(heightmap, start, middle, randomness//2)
+    diamondSquareAlgorithm(heightmap, (middle[0], start[1]), (end[0], middle[1]), randomness//2)
+    diamondSquareAlgorithm(heightmap, (start[0], middle[1]), (middle[0], end[0]), randomness//2)
+    diamondSquareAlgorithm(heightmap, middle, end, randomness//2)
 
 
-diamondSquareAlgorithm(([0,0, ], ))
+diamondSquareAlgorithm(heightmap)
